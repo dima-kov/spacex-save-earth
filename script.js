27b1c5792ddd240bd212a9d895c2db8b758e22eb
Dima Kovalchuk <dmyutro@ukr.net>
2017-09-10 10:27:59 +0300

    Add earth

--- script.js
+++ script.js
@@ -3,11 +3,18 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var ctx = canvas.getContext("2d");
-var rocketImage = new Image()
-var rocketWidth = '60';
-var rocketHeight = '60';
+
+var rocketImage = new Image();
+var earthImage = new Image();
+var rocketWidth = 60;
+var rocketHeight = 60;
+
+var earthWidth = canvas.width;
+var earthHeight = 100;
 var rocketX = (canvas.width - rocketWidth) / 2;
-var rocketY = canvas.height - rocketHeight;
+var rocketY = canvas.height - earthHeight - rocketHeight;
+var earthX = 0;
+var earthY = canvas.height - earthHeight;
 
 // Buttons
 var rightPressed = false;
@@ -20,14 +27,21 @@ var key = {
     RIGHT: 39,
     DOWN: 40,
 }
-var speed = 1;
+var speed = 2;
+
+
+var rocketMoveY = 0;
+var rocketMoveX = 0;
+
 
 rocketImage.addEventListener('load', drawRocket(), false);
-rocketImage.src = 'images/rocket.png'
+rocketImage.src = 'images/rocket.png';
+
+earthImage.addEventListener('load', drawEarth(), false);
+earthImage.src = 'images/earth.png';
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
-
 
 function keyDownHandler(e) {
     if(e.keyCode == key.RIGHT) {
@@ -58,30 +72,53 @@ function keyUpHandler(e) {
     }
 }
 
+
+function collisionWithEarth(){
+    var left = rocketX + rocketMoveX + rocketWidth > earthX;
+    var right = rocketX + rocketMoveX < earthX + earthWidth;
+    var top = rocketY + rocketHeight + rocketMoveY - 20 > earthY;
+    if (left && right && top) {
+        return true;
+    }
+    return false;
+}
+
 function trackKeys() {
-    if (rightPressed && rocketX < canvas.width - rocketWidth) {
-        rocketX += speed
+    rocketMoveX = 0;
+    rocketMoveY = 0;
+    if (rightPressed && rocketX + speed < canvas.width - rocketWidth) {
+        rocketMoveX += speed
     }
-    else if(leftPressed && rocketX > 0) {
-        rocketX -= speed
+    else if(leftPressed && rocketX - speed > 0) {
+        rocketMoveX -= speed
     }
-    if(upPressed && rocketY > 0) {
-        rocketY -= speed
+    if(upPressed && rocketY - speed > 0) {
+        rocketMoveY -= speed
     }
-    else if(downPressed && rocketY < canvas.height - rocketHeight) {
-        rocketY += speed
+    else if(downPressed && rocketY + speed < canvas.height - rocketHeight) {
+        rocketMoveY += speed
     }
 }
 
 function drawRocket() {
-    ctx.beginPath();
     trackKeys();
-    ctx.drawImage(rocketImage, rocketX, rocketY)
-    ctx.closePath();
+    if (!collisionWithEarth()) {
+        rocketX += rocketMoveX;
+        rocketY += rocketMoveY;
+    }
+    ctx.drawImage(rocketImage, rocketX, rocketY);
 }
 
+function drawEarth() {
+    ctx.beginPath();
+    ctx.rect(earthX, earthY, earthWidth, earthHeight);
+    ctx.fillStyle = 'green';
+    ctx.fill();
+    ctx.stroke();
+}
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawEarth();
     drawRocket();
 }
 
